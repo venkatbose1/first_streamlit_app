1ec719c58c4958d68709543b06e59e1e5a3e601f
venkatbose1 <149604519+venkatbose1@users.noreply.github.com>
2023-11-02 14:20:47 +0530

    Update streamli_app.py
    Change the header text and add Emojis to the menu items for mom's.

--- streamli_app.py
+++ streamli_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
+streamlit.header('My Mom's New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
