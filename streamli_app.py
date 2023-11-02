8d63b080c2ba1b6cc433673f202d22e547679541
venkatbose1 <149604519+venkatbose1@users.noreply.github.com>
2023-11-02 20:01:28 +0530

    Update streamli_app.py
    Pandas

--- streamli_app.py
+++ streamli_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('My Moms New Healthy Diner')
+streamlit.header("My Mom's New Healthy Diner")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
