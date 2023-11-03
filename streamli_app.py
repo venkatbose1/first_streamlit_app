803658be11ad05ec16c9cb5b9bec20783e29e93c
venkatbose1 <149604519+venkatbose1@users.noreply.github.com>
2023-11-03 18:07:32 +0530

    Update streamli_app.py
    Choose a Few Fruits to Set a Good Example

--- streamli_app.py
+++ streamli_app.py
@@ -16,6 +16,6 @@ streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","strawberries"])
 
 
