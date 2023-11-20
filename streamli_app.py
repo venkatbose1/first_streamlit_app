6f947e876b56853d396281566cfbaaedb3fa84b8
venkatbose1 <149604519+venkatbose1@users.noreply.github.com>
2023-11-20 12:59:42 +0530

    Update streamli_app.py
    Create Some User Interaction

--- streamli_app.py
+++ streamli_app.py
@@ -1,13 +1,7 @@
 import pandas
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header("My Mom's New Healthy Diner")
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
